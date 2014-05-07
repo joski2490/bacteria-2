2526a1d11d7e1713a4a79f3fa7d1bfd781f00ba6
shavounet <valentin@clavreul.com>
2014-05-07 14:06:25 +0200

    Basic lifeform enabled

--- bacteria.js
+++ bacteria.js
@@ -9,17 +9,25 @@ function generateBacteria(x,y)
 
     var dx = Math.random() - 0.5;
     var dy = Math.random() - 0.5;
+    var health = 150;
+    var living = true;
 
     return {
         draw: function (ctx){
             ctx.fillRect(x-1,y-1,2,2);
         },
         move: function(){
-            x += dx;
-            y += dy;
-            dx = Math.random() - 0.5;
-            dy = Math.random() - 0.5;
+            if(health<=0){
+                living = false;
+            } else {
+                health -= 1;
+                x += dx;
+                y += dy;
+                dx = Math.random() - 0.5;
+                dy = Math.random() - 0.5;
+            }
         },
-        generateBacteria: function(){return generateBacteria(x,y)}
+        generateBacteria: function(){return generateBacteria(x,y)},
+        isDead: function(){return !living;}
     };
 }
