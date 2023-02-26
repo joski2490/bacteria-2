c35582b465aca29e40b2a39f5de70fe070ad2462
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-02-26 03:10:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ window.addEventListener("load", function (event){
 */
         stats.innerHTML = "";
         if(play_chkb.checked){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.clearRect(0, 0, canvas.width*2, canvas.height*2);
             environment.mainLoop(ctx);
         }
 
